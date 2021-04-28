2a7c044ed464c7e3985aad1314efaf48ba8e6e5c
Jyotisubhra Ghosh-CIC Germany <jyotisubhra.ghosh-cic.germany@ibm.com>
2021-04-28 16:54:11 +0200

    first commit

---
+++ src/main/java/com/nb/jworld/CurrencyExchangeSvcApplication.java
@@ -0,0 +1,13 @@
+package com.nb.jworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nb/jworld/CurrencyExchangeSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.jworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
